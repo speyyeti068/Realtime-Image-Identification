d10f36bc1d8116f16fe99cc1b94967630c8ff123
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-04-02 18:41:17 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,14 @@
+// https://teachablemachine.withgoogle.com/models/t1BE4jTP-/
+
+function preload(){
+
+}
+
+function setup(){
+    canvas = createCanvas(300,300);
+    canvas.center();
+}
+
+function draw(){
+
+}
