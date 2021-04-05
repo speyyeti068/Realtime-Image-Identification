85b04af492865b3af6d1ea364b1b6f34ee8d924a
speyyeti068 <75049648+speyyeti068@users.noreply.github.com>
2021-04-05 18:48:09 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,14 +1,30 @@
-// https://teachablemachine.withgoogle.com/models/t1BE4jTP-/
-
 function preload(){
 
 }
 
 function setup(){
-    canvas = createCanvas(300,300);
+    canvas = createCanvas(300,250);
     canvas.center();
+
+    video = createCapture(VIDEO);
+    video.hide()
+
+    classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/t1BE4jTP-/model.json",function(){
+        console.log("Model Loaded");
+    })
 }
 
 function draw(){
+    image(video,0,0,300,250);
 
+    classifier.classify(video,function(error,result){
+        if(error){
+            console.error(error);
+        } else {
+            console.log(result[0].label);
+
+            document.getElementById("object").innerHTML = result[0].label;
+            document.getElementById("accuracy").innerHTML = ((result[0].confidence * 100).toFixed(2) + "%");
+        }
+    });
 }
